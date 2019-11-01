8e981db8400dcaf4b61a97fb6870be913ca9a909
Yutee Okon <yuteenightxi@gmail.com>
2019-11-01 13:42:23 +0100

    update app 1-11-2019

--- intern-experience.php
+++ intern-experience.php
@@ -65,12 +65,12 @@
 
   
     <section class="jumb">  
-            <h2>Experience Recap From Past Interns</h2>
-            <p class="para">Testimonies From Ex HNG Internship Finalists</p>
-            <a href="#modal-id" data-toggle="modal" class="button-primary">Submit Experience</a> <!--button to add experience-->
+            <h2>Past Interns Experience</h2>
+            <p class="para">Testimonies From Ex-HNG Internship Finalists</p>
+            <p class="text-center" onclick="message()"><a href="#">Submit Experience</a></p>
    </section>
 
-    <!--modal here-->
+    
     <div class="modal fade" id="modal-id">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -134,10 +134,9 @@
                             }
                             ?>
                             <div class="wrapper-child"  >
-                                <img class="image" src="<?php echo $img_src;?>">
-                                <h4><?php echo $all_experience['names'];?></h4>
-                                <p class="stack"><?php echo $all_experience['stack'];?></p>
-                                <hr class="hline"/>
+                                <img class="image" src="<?php echo $img_src;?>" alt = "intern img">
+                                <h4 class="name"><?php echo $all_experience['names'];?></h4>
+                                <p class="stack"><?php echo $all_experience['stack'];?></p> <hr>
                                 <p class="experience">
                                     <?php echo nl2br($all_experience['experience']);?>
                                 </p>
@@ -164,5 +163,11 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
         crossorigin="anonymous"></script>
-    <script src="js/intern-experience.js"></script>
+    <script src="js/intern-experience.js">
+    </script>
+    <script>
+          function message (){
+            alert("Function under construction check back later!");
+        }
+    </script>
 </html>
